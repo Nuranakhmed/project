711f30c809d18b9b3ce4dc8286eaeec64825444b
abdulayef1 <7ka1cyr>
2022-12-01 12:39:20 +0400

    yarimciq

---
+++ MiniProject/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProject.Services
+{
+    internal class UserService:User
+    {
+        public void ShareStatus(Status value)
+        {
+            Statuses.Add(value);
+
+        }
+        public int GetStatusById(int? Id)
+        {
+
+            return
+    
+    }
+        public void List<Status>  GetAllStatuses()
+        {
+            List<Status> results = Status.FindAll();
+            if (results.Count != 0)
+            {
+                return results;
+            }
+            else
+            {
+                Console.WriteLine("\nNo found.");
+                return
+        }
+            public void FilterStatusByDate(DateTime time)
+            {
+                return
+            }
+
+        }
+    }
